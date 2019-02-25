13defa600df1e60a2aa954fb530efaee371c45a7
Andre Camilo <andrehcamilo@gmail.com>
2019-02-25 09:18:46 -0300

    alterado arquivo client

--- src/tcp/client.go
+++ src/tcp/client.go
@@ -14,8 +14,10 @@ func main() {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Text to send: ")
 		text, _ := reader.ReadString('\n')
+
 		// send to socket
 		fmt.Fprintf(conn, text+"\n")
+
 		// listen for reply
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 		fmt.Print("Message from server: " + message)
